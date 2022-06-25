15ab00f1d5baace4a43df417d3cf1fe68d78966b
deVamshi <dev.vamshidhar@gmail.com>
2022-06-25 21:49:12 +0530

    added gh pages

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./index.css";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+      <div>
+        <h1 className="title">deVamshi</h1>
+        <hr className="divider" />
+        <p className="about">
+          a full stack developer with a passion... <strong> CRAP! </strong> I'll
+          just get to the point. I build software using the below technologies.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <p className="skills">ReactJs | Flutter | NodeJs</p>
+        {/* <button className="more-button">that's it?</button> */}
+        {/* <hr />
+      <p className="about">
+        I am nothing special, of this I am sure. I am a common man with common
+        thoughts and I've led a common life. There are no monuments dedicated
+        to me and my name will soon be forgotten, but I've loved another with
+        all my heart and soul, and to me, this has always been enough
+      </p>
+      <p className="skills">- Nicholas Sparks</p> */}
+      </div>
     </div>
   );
 }
